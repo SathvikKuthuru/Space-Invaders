e698e1c20251a81a159a41bd67fb3eea49ce1313
Sathvik Kuthuru <31018656+SathvikKuthuru@users.noreply.github.com>
2017-10-27 13:37:45 -0400

    Update BounceImage.java

--- Bouncing/BounceImage.java
+++ Bouncing/BounceImage.java
@@ -1,1 +1,35 @@
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
 
+public class BounceImage {
+	
+	double x, y;
+	double vx = Math.random();
+	double vy = Math.random();
+	
+	Image img = Toolkit.getDefaultToolkit().getImage("");
+	
+	
+	public BounceImage(int x, int y) {
+		this.x = (int) x;
+		this.y = (int) y;
+	}
+	
+	
+	
+	
+	public void update(){
+		
+	}
+	
+	public void paint(Graphics g){
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
