84c1c2cf6ba54fd8ab12ebd8b034a5eda375c0ae
Sathvik Kuthuru <31018656+SathvikKuthuru@users.noreply.github.com>
2017-12-05 13:44:39 -0500

    Update Invader.java

--- Game/Invader.java
+++ Game/Invader.java
@@ -9,7 +9,8 @@ import javax.swing.JFrame;
 
 public class Invader extends JComponent implements Runnable, KeyListener {
 
-	ArrayList<Enemy> enemy = new ArrayList<Enemy>();
+	ArrayList<Enemy> enemy1 = new ArrayList<Enemy>();
+	ArrayList<Enemy> enemy2 = new ArrayList<Enemy>();
 	Player player = new Player();
 	Bullet bullet = new Bullet();
 	static boolean b = false;
@@ -18,25 +19,30 @@ public class Invader extends JComponent implements Runnable, KeyListener {
 	
 	public Invader() {
 		
-		int x = 70, y = 70;
+		int x = 0, y = 70;
 		
-		for(int i = 1; i < 3; i++){
-			for(int j = 2; j < 8; j++){
-				enemy.add(new Enemy(j*x, i*y));
-			}	
-			
+		for(int i = 0; i < 8; i++){
+			x += 70;
+			enemy1.add(new Enemy(x, y));
 		}
-		
+		x = 0;
+		y= 140;
+		for(int i = 0; i < 8; i++){
+			x += 70;
+			enemy2.add(new Enemy(x, y));
+		}
 
 		Thread t = new Thread(this);
 		t.start();
 	}
 
 	public void paint(Graphics g) {
-		for(Enemy e : enemy){
+		for(Enemy e : enemy1){
 			e.paint(g, this);
 		}
-		
+		for(Enemy e : enemy2){
+			e.paint(g, this);
+		}		
 		bullet.paint(g, this);
 		player.paint(g, this);
 		g.setColor(Color.green);
@@ -58,9 +64,83 @@ public class Invader extends JComponent implements Runnable, KeyListener {
 	@Override
 	public void run() {
 		while (true) {
-			for(Enemy e : enemy){
+			int x = 0;
+			int y = 70;
+			if(enemy1.size() == 0 && enemy2.size() == 0){
+				level++;
+				for(int i = 0; i < 8; i++){
+					x += 70;
+					enemy1.add(new Enemy(x, y));
+				}
+				x = 0;
+				y= 140;
+				for(int i = 0; i < 8; i++){
+					x += 70;
+					enemy2.add(new Enemy(x, y));
+				}
+				for(Enemy e : enemy1){
+					e.vx++;
+				}
+				for(Enemy e : enemy2){
+					e.vx++;
+				}
+			} 
+			
+			for(Enemy e : enemy1){
 				e.update();
-			}if(enemy.size() > 0){  
+			}
+			for(Enemy e : enemy2){
+				e.update();
+			}
+			
+			
+			if(enemy1.size() > 0 && (enemy1.get(enemy1.size() - 1).reachedEdge(enemy1.get(enemy1.size()-1).x) || enemy1.get(0).reachedEdge(enemy1.get(0).x))){
+				for(int i = 0; i < enemy1.size(); i++){
+					enemy1.get(i).y += 50;
+					enemy1.get(i).vx = -enemy1.get(i).vx;
+				} 
+				for(int i = 0; i < enemy2.size(); i++){
+					enemy2.get(i).y += 50;
+					enemy2.get(i).vx = -enemy2.get(i).vx;
+				} 
+			}
+			
+			if(b){
+				bullet.update();
+				if(bullet.y <= 0) {
+					bullet.y = 600;
+					b = false;
+				}
+			}
+			else{
+				bullet.x = player.x + 37;
+			}
+			for(int i = 0; i < enemy1.size(); i++){
+				if(bullet.doesHit(enemy1.get(i).x, enemy1.get(i).y)){
+					enemy1.remove(enemy1.get(i));
+					bullet.x = player.x + 37;
+					bullet.y = 600;
+					b = false;
+					score += 100;
+					break;
+				}
+			}
+			for(int i = 0; i < enemy2.size(); i++){
+				if(bullet.doesHit(enemy2.get(i).x, enemy2.get(i).y)){
+					enemy2.remove(enemy2.get(i));
+					bullet.x = player.x + 37;
+					bullet.y = 600;
+					b = false;
+					score += 100;
+					break;
+				}
+			}
+			
+			
+			/*for(Enemy e : enemy){
+				e.update();
+			}
+			if(enemy.size() > 0){  
 				System.out.println(enemy.size());
 			if(enemy.get(enemy.size()/2).reachedEdge(enemy.get(enemy.size()/2).x) || enemy.get(0).reachedEdge(enemy.get(0).x)){
 				for(int i = 0; i < enemy.size(); i++){
@@ -69,7 +149,7 @@ public class Invader extends JComponent implements Runnable, KeyListener {
 				} 
 			}
 		}
-
+ 
 			for(int i = enemy.size()-1; i >= 0; i--){
 				if(b && !bullet.doesHit(enemy.get(i).x, enemy.get(i).y)){
 					bullet.update();
@@ -86,7 +166,8 @@ public class Invader extends JComponent implements Runnable, KeyListener {
 					score += 100;
 					break;
 				}
-			}
+			}*/
+			
 
 			repaint();
 			try {
